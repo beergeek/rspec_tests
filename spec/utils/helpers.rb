03856b382d18c07ac23a0a37d1720da1b3c2c20b
beergeek <brett.gray@puppetlabs.com>
2016-12-22 10:26:51 +1100

    better code for rspec 3

--- spec/utils/helpers.rb
+++ spec/utils/helpers.rb
@@ -7,7 +7,7 @@ shared_examples_for 'test_linux' do
       expect(catalogue.resources.select { |r| r.type == "File" && r[:mode] =~ /777/ }).to be_empty
     end
     it do
-      expect(catalogue).to contain_file('/tmp/clown').with({
+      is_expected.to contain_file('/tmp/clowns').with({
         'ensure' => 'file'
       })
     end
